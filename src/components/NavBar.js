ff8e61fbc3fc0a76f20e9cfc11ca58c47c7a4663
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2020-12-16 23:18:27 -0700

    update

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function NavBar() {
+    return (
+        <div className='navbar'>
+            <ul>
+                <Link className='link' to='/' exact >Home</Link>
+               
+            </ul>
+        </div>
+    )
+}
