47bb412a6f07f8a473f5fbbb707b84ba1560c640
KVlearn <kavya.mdu@gmail.com>
2020-12-27 23:11:07 -0800

    temp instructor login done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React,{useState} from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Route} from 'react-router-dom';
 import Home from './components/Home';
 import User from './components/User';
 import './App.css';
@@ -12,9 +12,7 @@ export default function App() {
     <div className= 'app'>
       <Route component={Home} path='/' exact/>
       <Route component={User} path='/user'/>
-      <Route path="/instructor">
-        <InstructorHome/>
-      </Route>
+      <Route path="/instructor" component={InstructorHome} />
       </div>
       </BrowserRouter>
   )
